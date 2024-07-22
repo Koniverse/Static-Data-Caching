846817203d66c654e9f7332a7b053a593445595d
S2kael <laiducminh1002@gmail.com>
2024-07-22 19:51:04 +0700

    Update web-runner url

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import {VirtualBrowser} from "./lib/VirtualBrowser.mjs";
 import {writeJSONFile} from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://17064b5f.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://26616626.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
