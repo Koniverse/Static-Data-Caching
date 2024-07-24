1a49e0082f30b74097788b8251e98769d40a46d1
S2kael <laiducminh1002@gmail.com>
2024-07-24 16:16:50 +0700

    Update web-runner url

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import {VirtualBrowser} from "./lib/VirtualBrowser.mjs";
 import {writeJSONFile} from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://58272d38.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://pr-3381--sw-web-runner.netlify.app/';
 
 console.log('Fetching data from', webRunnerURL);
 
