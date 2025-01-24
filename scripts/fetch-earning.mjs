5fef00136c5e684efd7e7e3070a3c14de1765242
S2kael <laiducminh1002@gmail.com>
2025-01-24 18:16:13 +0700

    Update earning data (dev)

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import {VirtualBrowser} from "./lib/VirtualBrowser.mjs";
 import {writeJSONFile} from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://72007ef8.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://01ae854a.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
