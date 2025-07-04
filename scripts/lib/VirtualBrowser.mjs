1ac8ed15449b75c21eef22897c5436a51f3f07f7
S2kael <laiducminh1002@gmail.com>
2025-07-04 15:43:12 +0700

    Update browser logic

--- scripts/lib/VirtualBrowser.mjs
+++ scripts/lib/VirtualBrowser.mjs
@@ -1,5 +1,7 @@
 import puppeteer from "puppeteer";
 
+const skipConsole = process.env.PUPPETEER_SKIP_CONSOLE === 'true' || process.env.PUPPETEER_SKIP_CONSOLE === '1';
+
 export class VirtualBrowser {
   browser;
 
@@ -22,14 +24,16 @@ export class VirtualBrowser {
     const browser = await this.getBrowser();
     const page = await browser.newPage();
 
-    // Listen for browser console messages
-    page.on('console', msg => {
-      const type = msg.type();
+    if (!skipConsole) {
+      // Listen for browser console messages
+      page.on('console', msg => {
+        const type = msg.type();
 
-      if (type === "error" || type === "debug") {
-        console.error(`[Browser Console] ${msg.type()}: ${msg.text()}`);
-      }
-    });
+        if (type === "error" || type === "debug") {
+          console.error(`[Browser Console] ${msg.type()}: ${msg.text()}`);
+        }
+      });
+    }
 
     await page.goto(url);
 
