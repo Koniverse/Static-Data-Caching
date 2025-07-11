9404666e814268e87aeb835c015fa7a6a37fba2f
tunghp2002 <atsimet@gmail.com>
2025-07-11 17:09:33 +0700

    Update earning data (dev)

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import {VirtualBrowser} from "./lib/VirtualBrowser.mjs";
 import {writeJSONFile} from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://70b7e588.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://f4341857.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
