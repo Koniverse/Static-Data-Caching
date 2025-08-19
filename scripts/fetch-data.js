b6913e4ffa8598d88b13ca083cc810beb72b9a64
tunghp2002 <atsimet@gmail.com>
2025-08-19 14:50:00 +0700

    feat: init fetch dtao validator data

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,10 +1,12 @@
 import {fetchEarning} from "./fetch-earning.mjs";
 import {fetchPrice} from "./fetch-price.js";
+import { fetchDtaoValidatorData } from "./fetch-dtao-validator.mjs";
 
 const main = async () => {
   await Promise.all([
     fetchEarning(),
     fetchPrice(),
+    fetchDtaoValidatorData()
   ])
 }
 
