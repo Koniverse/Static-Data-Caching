d68ab4f23303e01032641dd90d171a2a949d8a2c
PDTnhah <phamduythanh43@gmail.com>
2025-09-09 12:30:10 +0700

    Update earning data (dev)

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import { VirtualBrowser } from "./lib/VirtualBrowser.mjs";
 import { writeJSONFile } from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://2ab5480e.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://2cf035c7.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
