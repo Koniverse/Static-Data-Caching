6761fbd079e8637fc08e2baac55c39f9daf1f884
tunghp2002 <atsimet@gmail.com>
2025-10-20 18:59:13 +0700

    Update earning data (dev)

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import { VirtualBrowser } from "./lib/VirtualBrowser.mjs";
 import { writeJSONFile } from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://454e31ba.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://df499171.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
@@ -46,7 +46,8 @@ const runBrowser = async () => {
         manta: ['manta_network'],
         bittensor: ['bittensor', 'bittensor_testnet'],
         energy: ['energy_web_x_testnet', 'energy_web_x'],
-        mythos: ['mythos', 'muse_testnet']
+        mythos: ['mythos', 'muse_testnet'],
+        tanssi: ['tanssi', 'dancelight']
       };
 
       const enableChains = Array.from(new Set([
