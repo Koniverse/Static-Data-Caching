b053c66f83577cb36acf629fd2843ad23c1b14bf
Thiendekaco <139972251+Thiendekaco@users.noreply.github.com>
2025-10-21 15:13:36 +0700

    feat: update energy staking to show apy

--- scripts/fetch-earning.mjs
+++ scripts/fetch-earning.mjs
@@ -2,7 +2,7 @@ import { VirtualBrowser } from "./lib/VirtualBrowser.mjs";
 import { writeJSONFile } from "./lib/utils.mjs";
 import oldData from "../data/earning/yield-pools.json" assert {type: "json"};
 
-const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://df499171.swwrc.pages.dev/';
+const webRunnerURL = process.env.WEB_RUNNER_URL || 'https://f8277d2c.swwrc.pages.dev/';
 
 console.log('Fetching data from', webRunnerURL);
 
